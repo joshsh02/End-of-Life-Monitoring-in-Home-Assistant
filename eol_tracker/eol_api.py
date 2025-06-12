59d6a3e0495123f12b99439071cfac8d974a04ac
joshsh02 <joshnoodlee@gmail.com>
2025-06-12 13:32:30 -0400

    Delete eol_tracker/eol_api.py

--- eol_tracker/eol_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-products = requests.get("https://endoflife.date/api/all.json").json()
-
-for product in products[:10]:
-    response = requests.get(f"https://endoflife.date/api/{product}.json").json()
-
-    for entry in response:
-        print(f"{product} EOL dates: {entry['cycle']}: {entry['eol']}")
